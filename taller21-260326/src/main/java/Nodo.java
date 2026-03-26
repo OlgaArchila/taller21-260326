a8881e323bcb5faefc6fc7bbba42ac46990f21ac
Olga Guacaneme <est.olga.guacaneme@unimilitar.edu.co>
2026-03-26 10:09:49 -0500

    Se creo la clase Nodo con el instructor de nodo

---
+++ taller21-260326/src/main/java/Nodo.java
@@ -0,0 +1,12 @@
+
+public class Nodo {
+    int dato; // guarda el número
+    Nodo siguiente; // referencia al siguiente nodo
+
+    // constructor del nodo
+    public Nodo(int dato) {
+        this.dato = dato; // asigna el valor al nodo
+        this.siguiente = null; // inicialmente no apunta a otro nodo
+    }
+}
+
---
+++ taller21-260326/src/main/java/Taller21260326.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+
+ */
+public class Taller21260326 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
