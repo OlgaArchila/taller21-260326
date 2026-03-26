615a24bb656eb6147ee457a99365584d6b68545f
Olga Guacaneme <est.olga.guacaneme@unimilitar.edu.co>
2026-03-26 10:52:05 -0500

    Se esta realizando el metodo para ordenar la lista

--- taller21-260326/src/main/java/Lista.java
+++ taller21-260326/src/main/java/Lista.java
@@ -18,10 +18,35 @@ public class Lista {
                 aux = aux.siguiente; // avanzar al siguiente nodo
             }
 
-       
-     
+        
+        }
+        
     }
+    public class ordenar {
+    
+    
+    Nodo actual = cabeza; // nodo que recorrerá la lista
+
+    while (actual != null) { // mientras existan nodos
+
+        Nodo siguiente = actual.siguiente; // nodo siguiente
+
+        while (siguiente != null) { // recorrer los demás nodos
+
+            if (actual.dato > siguiente.dato) { // si están desordenados
+
+                int temp = actual.dato; // guardar valor temporal
+                actual.dato = siguiente.dato; // intercambiar valores
+                siguiente.dato = temp; // completar intercambio
+            }
+
+            siguiente = siguiente.siguiente; // avanzar
+        }
+
+        actual = actual.siguiente; // pasar al siguiente nodo
     }
+}
+}
 }
 
 
---
+++ taller21-260326/src/main/java/ordenar.java
@@ -0,0 +1,27 @@
+
+public class ordenar {
+ /*   
+    
+    Nodo actual = cabeza; // nodo que recorrerá la lista
+
+    while (actual != null) { // mientras existan nodos
+
+        Nodo siguiente = actual.siguiente; // nodo siguiente
+
+        while (siguiente != null) { // recorrer los demás nodos
+
+            if (actual.dato > siguiente.dato) { // si están desordenados
+
+                int temp = actual.dato; // guardar valor temporal
+                actual.dato = siguiente.dato; // intercambiar valores
+                siguiente.dato = temp; // completar intercambio
+            }
+
+            siguiente = siguiente.siguiente; // avanzar
+        }
+
+        actual = actual.siguiente; // pasar al siguiente nodo
+*/
+    }
+
+
